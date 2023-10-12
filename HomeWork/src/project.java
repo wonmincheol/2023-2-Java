8a6b611c5fa9a5bcac12a11deaf231f7ced08b62
won mincheol <59256157+wonmincheol@users.noreply.github.com>
2023-10-12 23:41:53 +0900

    Update project.java

--- HomeWork/src/project.java
+++ HomeWork/src/project.java
@@ -5,17 +5,25 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JRadioButton;
-import javax.swing.border.Border;
+
 import java.util.*;
 import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+//DnD방식 구현 코드 실패(데이터 변환시 발생하는 형변환 오류)
+//https://www.specialagentsqueaky.com/blog-post/mbu5p27a/2011-01-09-drag-and-dropping-files-to-java-desktop-application/
+
+
+
 class jp_lock extends JPanel{
     JLabel file_path = new JLabel();
-
+    File selectFile;
+    JProgressBar bar;
     public jp_lock(){
         this.setVisible(true);
         this.setLayout(new BorderLayout());
@@ -23,8 +31,22 @@ class jp_lock extends JPanel{
         JButton file_load_Button = new JButton("load file(lock)");
         file_load_Button.addActionListener(new Load_File());
         
+        file_path.setBackground(new Color(255,255,255));
+        file_path.setOpaque(true);
+
+        JButton run_Button = new JButton("run(lock)");
+        run_Button.addActionListener(new run());
+        
+        bar = new JProgressBar();
+        bar.setValue(1);
+        bar.setStringPainted(true);
+
+
         this.add(file_load_Button,BorderLayout.WEST);
         this.add(file_path,BorderLayout.CENTER);
+        this.add(run_Button,BorderLayout.EAST);
+        this.add(bar,BorderLayout.SOUTH);
+
     }
 
     private class Load_File implements ActionListener{
@@ -34,17 +56,36 @@ class jp_lock extends JPanel{
             JFileChooser fileChooser = new JFileChooser();
             int result = fileChooser.showOpenDialog(window);
             if(result==JFileChooser.APPROVE_OPTION){
-                File selectFile = fileChooser.getSelectedFile();
+                selectFile = fileChooser.getSelectedFile();
                 
-                //파일 경로@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                 file_path.setText(selectFile.toString());
             }
+        }
+    }
+    private class run implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            //파일 경로@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+            System.out.println(selectFile.toString());
+            //진행상황 표시
+            bar_setValue(-1);
+        }
+    }
+
+    public void bar_setValue(int v){
+        if(v==-1){
+            int val=bar.getValue();
+            bar.setValue(++val);
+        }
+        else{
+            bar.setValue(v);
         }
     }
 }
 
 class jp_unlock extends JPanel{
     JLabel file_path = new JLabel();
+    File selectFile;
+    JProgressBar bar;
 
     public jp_unlock(){
         this.setVisible(true);
@@ -53,8 +94,21 @@ class jp_unlock extends JPanel{
         JButton file_load_Button = new JButton("load file(unlock)");
         file_load_Button.addActionListener(new Load_File());
         
+        file_path.setBackground(new Color(255,255,255));
+        file_path.setOpaque(true);
+
+        JButton run_Button = new JButton("run(unlock)");
+        run_Button.addActionListener(new run());
+        
+        bar = new JProgressBar();
+        bar.setValue(1);
+        bar.setStringPainted(true);
+        
         this.add(file_load_Button,BorderLayout.WEST);
         this.add(file_path,BorderLayout.CENTER);
+        this.add(run_Button,BorderLayout.EAST);
+        this.add(bar,BorderLayout.SOUTH);
+
     }
 
     private class Load_File implements ActionListener{
@@ -64,14 +118,31 @@ class jp_unlock extends JPanel{
             JFileChooser fileChooser = new JFileChooser();
             int result = fileChooser.showOpenDialog(window);
             if(result==JFileChooser.APPROVE_OPTION){
-                File selectFile = fileChooser.getSelectedFile();
+                selectFile = fileChooser.getSelectedFile();
                 
-                //파일 경로@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                 file_path.setText(selectFile.toString());
             }
         }
     }
+    private class run implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            //파일 경로@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+            System.out.println(selectFile.toString());
+            //진행상황 표시
+            bar_setValue(-1);
+        }
+    }
+    public void bar_setValue(int v){
+        if(v==-1){
+            int val=bar.getValue();
+            bar.setValue(++val);
+        }
+        else{
+            bar.setValue(v);
+        }
+    }
 }
+
 class GUI extends JFrame{
 
     
@@ -106,7 +177,7 @@ class GUI extends JFrame{
         top_Panel.add(rb_lock);
         top_Panel.add(rb_unlock);
        
-        setSize(500, 150);
+        setSize(700, 150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setTitle("Encryption");
@@ -156,21 +227,7 @@ class GUI extends JFrame{
 }
 
 
-JTextArea myPanel = new JTextArea();
-myPanel.setDropTarget(new DropTarget() {
-    public synchronized void drop(DropTargetDropEvent evt) {
-        try {
-            evt.acceptDrop(DnDConstants.ACTION_COPY);
-            List<File> droppedFiles = (List<File>)
-                evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
-            for (File file : droppedFiles) {
-                // process files
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-});
+
 
 
 public class project {
