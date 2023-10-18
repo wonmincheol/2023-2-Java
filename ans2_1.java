4d4f9fe180d5924272c94036188f9471b7666e84
won mincheol <59256157+wonmincheol@users.noreply.github.com>
2023-10-18 10:43:58 +0900

    Add files via upload

--- ans2_1.java
+++ ans2_1.java
@@ -1,16 +1,67 @@
-package test;
-import java.util.*;
+class Account{
+    private int total = 0;
+    synchronized void deposit(){
+        if(trg==false){
+            total +=1000;
+        }
 
+    }
+    private boolean trg =false;
+    synchronized boolean tr(){
+        return trg;
+    }
+    synchronized void set_tr(){
+        trg=true;
+    }
+    int gettotal(){
+        return total;
+    }
+}
+
+class Customer extends Thread{
+    Account same_a;
+    
+    Customer(Account a, String s){
+        same_a = a;
+        setName(s);
+        
+    }
+    public void run(){
+        for(int i=1;i<=200;i++){
+            if((same_a.tr()==true)||(same_a.gettotal()>=500000)){
+                same_a.set_tr();
+                break;
+            }
+            else if(same_a.tr()==false){
+                System.out.println(getName()+" : "+i+"번째");
+                same_a.deposit();
+            }
+            
+        }
+    }
+}
+class runCus extends Customer implements Runnable{
+    public runCus(Account a, String s){
+        super(a, s);
+    }
+
+}
 public class ans2_1 {
-	public static void main(String args[]) {
-		String[] stringNumber = {"23","12","3.141592"};
-		try {
-			for(int i=0;i<stringNumber.length;i++) {
-				System.out.println("Value converted to integer: "+Integer.parseInt(stringNumber[i]));
-			}
-		}
-		catch(NumberFormatException e){
-			System.out.println(e.getMessage()+"cannot be converted to integer.");
-		}
-	}
+    public static void main(String args[]) throws InterruptedException{
+        Account same_account = new Account();
+
+        runCus dona1 = new runCus(same_account, "1번째 성금자");
+        runCus dona2 = new runCus(same_account, "2번째 성금자");
+        runCus dona3 = new runCus(same_account, "3번째 성금자");
+
+        dona1.start();
+        dona2.start();
+        dona3.start();
+        dona1.join();
+        dona2.join();
+        dona3.join();
+
+        System.out.println("성금 총액은 : "+same_account.gettotal());
+        
+    }
 }
--- ans2_2.java
+++ ans2_2.java
@@ -1,54 +1,28 @@
-package test;
-import java.util.*;
+class Bank{
+    int money;
+    public Bank(){
+        money=0;
+    }
+    synchronized int inmoney(int m){
+        money+=m;
+        notifyAll();
+        return money;
+        
+    }
+    synchronized int outmoney(int m){
+        try{
+            while(money<m){
+                wait();
+            }
+        }catch(InterruptedException e){}
+        return money;
+    }
+    
+}
+class User extends Thread{
+    
+}
 
 public class ans2_2 {
-	public static void main(String args[]) {
-		int first=0,last=99;
-		int ans = (int)(Math.random()*100);
-		int in;
-		int trynum=1;
-		Scanner sc = new Scanner(System.in);
-		while(true) {
-			System.out.println("Random number is generated. Guess the number:");
-			ans = (int)(Math.random()*100);
-			first=0;
-			last=99;
-			trynum=1;
-			while(true) {
-				try {
-					System.out.println(first + "-" + last);
-					System.out.print(trynum + ">>");
-					sc = new Scanner(System.in);
-					in = sc.nextInt();
-					
-					
-					if(in==ans) {
-						System.out.println("Correct.");
-						break;
-					}
-					else if(in>ans) {
-						System.out.println("Enter a low number");
-						last = in;
-					}
-					else {
-						System.out.println("Enter a high number");
-						first = in;
-					}
-					trynum++;
-					
-				}
-				catch(InputMismatchException e) {
-					System.out.println("Only integer is allowed.");
-				}
-			}
-			System.out.print("Try again (y/n)>>");
-			String input = sc.next();
-			if(input.equals("y")) {
-				continue;
-			}
-			else if(input.equals("n")) {
-				break;
-			}
-		}
-	}
+    
 }
